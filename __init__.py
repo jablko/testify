b80479a7a6169541b46f27271a8b3b46837400c7
Jack Bates <ms419@freezone.co.uk>
2011-06-09 08:05:51 -0700

    Add equiv()

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import os, re, sys, traceback
 
-__all__ = 'equal', 'expect', 'ok', 'test'
+__all__ = 'equal', 'equiv', 'expect', 'ok', 'test'
 
 def equal(expect, actual):
   current.count += 1
@@ -18,6 +18,124 @@ def equal(expect, actual):
     #print re.sub('^', '# ', repr(expect), flags=re.M)
     print re.compile('^', re.M).sub('# ', repr(expect))
 
+    traceback.print_stack(sys._getframe().f_back, file=sys.stdout)
+
+  return condition
+
+def equiv(expect, actual):
+  current.count += 1
+
+  def equiv(expect, actual, condition):
+    if expect is actual:
+      return condition
+
+    # Strings are recursive, 'a' == 'a'[0] == 'a'[0][0], etc. but equivalent
+    # strings are identical, 'a' is 'a'.  Strings are common and we can't
+    # determine nonequivalence without this special case.  Could this case be
+    # more general, like recursive values are nonequivalent unless identical?
+    # What about a = [], a.append(a), b = [], b.append(b), equiv(a, b)?  Maybe
+    # they're equivalent because they both reference themselves? but then 'a'
+    # and 'b' - and hence all equal length strings - are equivalent : (
+    if isinstance(actual, str) or not isinstance(actual, type(expect)):
+      if condition:
+        #print 'not ok {!r}'.format(current.count)
+        print 'not ok {0!r}'.format(current.count)
+
+      #print re.sub('^', '# ', repr(actual), flags=re.M)
+      print re.compile('^', re.M).sub('# ', repr(actual))
+      #print re.sub('^', '# ', repr(expect), flags=re.M)
+      print re.compile('^', re.M).sub('# ', repr(expect))
+
+      return False
+
+    try:
+      return equiv(expect.__dict__, actual.__dict__, condition)
+
+    except AttributeError:
+      try:
+
+        # Without **, ((1, 2), (3, 4)) and ((3, 4), (1, 2)) would be
+        # equivalent, and 'abc' would raise ValueError
+        expect = dict(**expect)
+        actual = dict(**actual)
+
+      except TypeError:
+        try:
+          expect = iter(expect)
+          actual = iter(actual)
+
+        except TypeError:
+          if condition:
+            #print 'not ok {!r}'.format(current.count)
+            print 'not ok {0!r}'.format(current.count)
+
+          #print re.sub('^', '# ', repr(actual), flags=re.M)
+          print re.compile('^', re.M).sub('# ', repr(actual))
+          #print re.sub('^', '# ', repr(expect), flags=re.M)
+          print re.compile('^', re.M).sub('# ', repr(expect))
+
+          return False
+
+        while True:
+          try:
+            condition = equiv(expect.next(), actual.next(), condition)
+
+          except StopIteration:
+            break
+
+        for itm in actual:
+          if condition:
+            #print 'not ok {!r}'.format(current.count)
+            print 'not ok {0!r}'.format(current.count)
+
+          #print re.sub('^', '# ', repr(itm), flags=re.M)
+          print re.compile('^', re.M).sub('# ', repr(itm))
+
+          condition = False
+
+        for itm in expect:
+          if condition:
+            #print 'not ok {!r}'.format(current.count)
+            print 'not ok {0!r}'.format(current.count)
+
+          #print re.sub('^', '# ', repr(itm), flags=re.M)
+          print re.compile('^', re.M).sub('# ', repr(itm))
+
+          condition = False
+
+        return condition
+
+      for key in set(expect) & set(actual):
+        condition = equiv(expect[key], actual[key], condition)
+
+      for key in set(actual) - set(expect):
+        if condition:
+          #print 'not ok {!r}'.format(current.count)
+          print 'not ok {0!r}'.format(current.count)
+
+        #print re.sub('^', '# ', repr(actual[key]), flags=re.M)
+        print re.compile('^', re.M).sub('# ', repr(actual[key]))
+
+        condition = False
+
+      for key in set(expect) - set(actual):
+        if condition:
+          #print 'not ok {!r}'.format(current.count)
+          print 'not ok {0!r}'.format(current.count)
+
+        #print re.sub('^', '# ', repr(expect[key]), flags=re.M)
+        print re.compile('^', re.M).sub('# ', repr(expect[key]))
+
+        condition = False
+
+      return condition
+
+  condition = equiv(expect, actual, True)
+  if condition:
+    #print 'ok {!r}'.format(current.count)
+    print 'ok {0!r}'.format(current.count)
+
+  else:
     traceback.print_stack(sys._getframe().f_back, file=sys.stdout)
 
   return condition
